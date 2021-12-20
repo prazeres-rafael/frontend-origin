f1c93fdec2ad94556bf198b5da78ecf55f317c43
Rafael Prazeres da Silva <rafael.prazeres@arquivei.com.br>
2021-12-20 09:42:11 -0300

    feat: add SavingGoal feature
    This commit add SavingGoal feature to aplication, this feature use
    the components of aplication.

---
+++ src/features/SavingGoal/SavingGoal.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Card from '../../components/Card';
+import BuyHouse from '../../assets/svg/buy-a-house.svg';
+import {
+  AmountDescription,
+  AmountParagraph,
+  AmountSection,
+  AmountValue,
+  ConfirmSection,
+  GoalSection,
+  Paragraph,
+  PaperBase,
+  ResultSection,
+  Title,
+  SavingGoalParagraph,
+  Section,
+  Subtitle,
+} from './styled';
+import { Box } from '@mui/material';
+import Input from '../../components/Input';
+import MonthPicker from '../../components/MonthPicker';
+import Cta from '../../components/Cta';
+
+export default function Header(): React.ReactElement {
+  return (
+    <Section>
+      <SavingGoalParagraph>
+        Let‘s plan your <b>saving goal.</b>
+      </SavingGoalParagraph>
+      <Card>
+        <GoalSection>
+          <BuyHouse />
+          <Box>
+            <Title>Buy a house</Title>
+            <Subtitle>Saving goal</Subtitle>
+          </Box>
+        </GoalSection>
+        <AmountSection>
+          <Box>
+            <Paragraph>Total amount</Paragraph>
+            <Input />
+          </Box>
+          <Box>
+            <Paragraph>Reach goal by</Paragraph>
+            <MonthPicker />
+          </Box>
+        </AmountSection>
+        <ResultSection>
+          <PaperBase elevation={0}>
+            <Box>
+              <AmountParagraph>Monthly amount</AmountParagraph>
+              <AmountValue>$520.83</AmountValue>
+            </Box>
+            <AmountDescription>
+              You‘re planning <b>48 monthly deposits</b> to reach your
+              <b> $25,000</b> goal by <b>October 2020</b>.
+            </AmountDescription>
+          </PaperBase>
+          <ConfirmSection>
+            <Cta>Confirm</Cta>
+          </ConfirmSection>
+        </ResultSection>
+      </Card>
+    </Section>
+  );
+}
---
+++ src/features/SavingGoal/styled.ts
@@ -0,0 +1,120 @@
+import styled from 'styled-components';
+import { Paper } from '@mui/material';
+import { palette } from '../../utils/palette';
+
+export const SavingGoalParagraph = styled.p({
+  color: palette.brand.primary,
+  fontSize: 20,
+  fontWeight: 500,
+  paddingTop: 48,
+  marginBottom: 24,
+});
+
+export const Title = styled.p({
+  color: palette.paragraph.primary,
+  fontSize: 24,
+  fontWeight: 800,
+  margin: 0,
+});
+
+export const Subtitle = styled.p({
+  color: palette.paragraph.secundary,
+  fontSize: 16,
+  fontWeight: 400,
+  margin: '10px 0 0 0',
+});
+
+export const AmountParagraph = styled.p({
+  color: palette.paragraph.primary,
+  fontSize: 20,
+  fontWeight: 400,
+  marginLeft: 32,
+});
+
+export const AmountValue = styled.p({
+  color: palette.amonthValue.default,
+  margin: '24px 30.5px 16px',
+  fontSize: 32,
+  fontWeight: 500,
+  textAlign: 'right',
+});
+
+export const AmountDescription = styled.p({
+  color: palette.paragraph.primary,
+  margin: '24px 32px',
+  fontSize: 12,
+  fontWeight: 400,
+});
+
+export const Paragraph = styled.p({
+  color: palette.paragraph.primary,
+  fontSize: 14,
+  fontWeight: 400,
+  margin: '0 0 6px 0',
+});
+
+export const Section = styled.section({
+  backgroundColor: palette.brand.secundary,
+  height: '90vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+export const GoalSection = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+
+  '& > div': {
+    marginLeft: 16,
+  },
+});
+
+export const AmountSection = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+  marginTop: '28px',
+
+  '& > div': {
+    width: '50%',
+  },
+
+  '& > div:last-child': {
+    marginLeft: 16,
+  },
+
+  '& > div:nth-child(1) > div > div': {
+    fontFamily: 'Rubik, normal',
+    fontSize: 24,
+    lineHeight: '120%',
+    fontWeight: 500,
+  },
+
+  '#input-with-icon-textfield': {
+    padding: '10px 0',
+  },
+});
+
+export const ResultSection = styled.div({
+  marginTop: '24px',
+});
+
+export const PaperBase = styled(Paper)({
+  border: `1px solid ${palette.border.default}`,
+  borderRadius: 8,
+  background: `linear-gradient(180deg, #FFF 50%, ${palette.brand.secundary} 50%)`,
+
+  '& > div': {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
+
+export const ConfirmSection = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  marginTop: '32px',
+});
