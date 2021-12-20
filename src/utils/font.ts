9c7e1dba57afb4318b573fd86c40ae18fba3aa8d
Rafael Prazeres da Silva <rafael.prazeres@arquivei.com.br>
2021-12-20 09:35:14 -0300

    feat: add utils folder to aplication
    This commit add utils folder to aplication, the folder contains:
    A file for golbal styles(fonts.ts).
    A file for svg paths traducer (path-transform.js).
    A file for palette of colors to aplication to make the maintenance
    of aplication a lot more easy and reusable.

---
+++ src/utils/font.ts
@@ -0,0 +1,10 @@
+export const fontFamily = 'Work Sans, sans-serif';
+
+export const font = `
+    html,
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: ${fontFamily};
+    }
+`;
---
+++ src/utils/palette.js
@@ -0,0 +1,16 @@
+export const palette = {
+  brand: {
+    primary: '#1B31A8',
+    secundary: '#F4F8FA',
+  },
+  paragraph: {
+    primary: '#1E2A32',
+    secundary: '#708797',
+  },
+  amonthValue: {
+    default: '#0079FF',
+  },
+  border: {
+    default: '#E9EEF2',
+  },
+};
