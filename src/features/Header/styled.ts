12128a3e938c0db2daa0bb8e7efd6aecc9183f97
Rafael Prazeres da Silva <rafael.prazeres@arquivei.com.br>
2021-12-20 09:37:57 -0300

    feat: add Header feature
    This commit add the header feature for aplication, this can be reused
    for others pages of aplication.

---
+++ src/features/Header/styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const HeaderOrigin = styled.header({
+  backgroundColor: '#FFF',
+  height: 56,
+  left: 0,
+  position: 'relative',
+  top: 0,
+  width: '100%',
+
+  '@media (min-width: 768px)': {
+    height: 80,
+  },
+});
